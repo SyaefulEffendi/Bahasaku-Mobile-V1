484722be5564345dc84f1b689012aa5c699df404
SyaefulEffendi <mohsyaefuleffendi@gmail.com>
2025-12-13 04:43:24 +0700

    IP untuk Android

--- lib/core/api/api_client.dart
+++ lib/core/api/api_client.dart
@@ -3,9 +3,10 @@ class ApiClient {
   // GANTI IP INI SESUAI IPCONFIG LAPTOP ANDA
   // Jangan pakai localhost untuk HP Fisik!
   //pake andorid studio:
-  static const String baseUrl = 'http://10.0.2.2:8080/api';
-  //pake hp fisik:
-  //static const String baseUrl = 'http://192.168.23.226:5000/api';
+  //static const String baseUrl = 'http://10.0.2.2:8080/api';
+  //pake hp fisik: gunakan Wireless LAN adapter Wi-Fi: IPv4 Address. . . . . . . . . . . :
+  static const String baseUrl = 'http://10.17.166.114:8080/api';
+
   
   // Endpoint Auth
   static const String login = '$baseUrl/users/login';
